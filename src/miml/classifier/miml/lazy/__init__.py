a57990f38e48cf73091e55607babac8c04ba1f6b
i62seroj <i62seroj@uco.es>
2026-06-07 13:48:40 +0200

    Update miml_mal_knn

--- src/miml/classifier/miml/lazy/__init__.py
+++ src/miml/classifier/miml/lazy/__init__.py
@@ -1,8 +1,10 @@
 from .miml_br_knn import MIMLBRkNN, ExtensionType
 from .miml_dgc import MIMLDGC
+from .miml_map_knn import MIMLMAPkNN
 
 __all__ = [ 
     "MIMLBRkNN",
-    "MIMLDGC"
+    "MIMLDGC",
+    "MIMLMAPkNN"
     "ExtensionType"
 ]
--- src/miml/classifier/miml/lazy/miml_map_knn.py
+++ src/miml/classifier/miml/lazy/miml_map_knn.py
@@ -1,64 +1,176 @@
 import numpy as np
 from collections import Counter
 
-from .ml_knn import MLkNN
+from .multi_instance_multi_label_knn import MultiInstanceMultiLabelKNN
+from ....core.average_hausdorff import AverageHausdorff
 
 
-class MIMLMAPkNN(MLkNN):
+class MIMLMAPkNN(MultiInstanceMultiLabelKNN):
 
     def __init__(self, k=10, metric=None, smooth=1.0):
 
-        super().__init__(k, metric, smooth)
+        super().__init__(k, metric)
+
+        if metric == None:
+            metric = AverageHausdorff()
+
+        self.k = k
+        self.metric = metric
+        self.smooth = smooth
+
+        self.bags = None
+        self.Y = None
+
+        self.D = None
+        self.prior = None
+        self.prior_n = None
+
+        self.cond = None
+        self.cond_n = None
+
+    def build_internal(self, training_set):
+
+        self.bags = list(training_set.data.values())
+
+        self.Y = np.array([
+            np.max(bag.get_labels(), axis=0)
+            for bag in self.bags
+        ])
+
+        self.fit(self.bags)
 
 
-    def make_prediction_internal(self, bag):
+    def fit(self, bags):
 
+        n = len(bags)
+
+        self.D = self.get_distances(bags)
+
+        n_labels = self.Y.shape[1]
+
+        self.prior = np.zeros(n_labels)
+        self.prior_n = np.zeros(n_labels)
+
+        self.cond = np.zeros((n_labels, self.k + 1))
+        self.cond_n = np.zeros((n_labels, self.k + 1))
+
+        self._compute_prior(n, n_labels)
+        self._compute_conditional(n, n_labels)
+
+    def _compute_prior(self, n, n_labels):
+        """
+        Compute prior probabilities.
+        """
+
+        for l in range(n_labels):
+
+            positives = np.sum(self.Y[:, l])
+
+            self.prior[l] = (self.smooth + positives) / (self.smooth * 2 + n)
+
+            self.prior_n[l] = 1.0 - self.prior[l]
+
+    def _compute_conditional(self, n, n_labels):
+        """
+        Compute conditional probabilities.
+        """
+
+        temp_c = np.zeros((n_labels, self.k + 1), dtype=int)
+
+        temp_nc = np.zeros((n_labels, self.k + 1), dtype=int)
+
+        for i in range(n):
+
+            neighbors = self._get_neighbors(i)
+
+            for l in range(n_labels):
+
+                aces = 0
+
+                for j in neighbors:
+
+                    if self.Y[j][l] == 1:
+                        aces += 1
+
+                if self.Y[i][l] == 1:
+                    temp_c[l][aces] += 1
+                else:
+                    temp_nc[l][aces] += 1
+
+        for l in range(n_labels):
+
+            total_c = np.sum(temp_c[l])
+            total_nc = np.sum(temp_nc[l])
+
+            for j in range(self.k + 1):
+
+                self.cond[l][j] = (self.smooth + temp_c[l][j]) / (self.smooth * (self.k + 1) + total_c)
+
+                self.cond_n[l][j] = (self.smooth + temp_nc[l][j]) / (self.smooth * (self.k + 1) + total_nc)
+
+
+
+    def _get_neighbors(self, i):
+        """
+        Get k nearest neighbours of training bag i.
+        """
+
+        distances = []
+
+        for j in range(len(self.bags)):
+
+            if i == j:
+                continue
+
+            distances.append((j, self.D[i][j]))
+
+        distances.sort(
+            key=lambda x: x[1]
+        )
+
+        return [
+            idx
+            for idx, _ in distances[:self.k]
+        ]
+
+    def predict(self, bag):
+        
         distances = []
 
         for i in range(len(self.bags)):
 
-            d = self.metric.distance(bag, self.bags[i])
+            d = self.metric.distance(bag,self.bags[i])
 
             distances.append((i, d))
 
-        distances.sort(key=lambda x: x[1])
+        distances.sort(
+            key=lambda x: x[1]
+        )
 
         neighbors = distances[:self.k]
 
+        n_labels = self.Y.shape[1]
 
-        labelsets = []
+        prediction = np.zeros(n_labels, dtype=int)
 
-        for idx, _ in neighbors:
+        confidence = np.zeros(n_labels, dtype=float)
 
-            labelsets.append(tuple(self.Y[idx].astype(int)))
+        for l in range(n_labels):
 
-        counts = Counter(labelsets)
+            aces = 0
 
-        best_labelset = None
-        best_score = -np.inf
+            for idx, _ in neighbors:
 
-        for labelset, count in counts.items():
+                if self.Y[idx][l] == 1:
+                    aces += 1
 
-            prior = self._labelset_prior(labelset)
+            p1 = (self.prior[l] * self.cond[l][aces])
+            p0 = (self.prior_n[l] * self.cond_n[l][aces])
 
-            likelihood = count / self.k
+            if p1 > p0:
+                prediction[l] = 1
 
-            score = prior * likelihood
+            if (p1 + p0) > 0:
+                confidence[l] = (p1 / (p1 + p0))
 
-            if score > best_score:
-
-                best_score = score
-                best_labelset = labelset
-
-        return np.array(best_labelset)
-
-    def _labelset_prior(self, labelset):
-
-        matches = np.sum([
-            np.all(self.Y[i] == labelset)
-            for i in range(len(self.Y))
-        ])
-
-        return (matches + self.smooth) / (
-            len(self.Y) + self.smooth
-        )
+        return prediction, confidence
