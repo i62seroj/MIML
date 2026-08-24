dba0bbec86211f5f8db4df31e998884b5c45f3e8
i62seroj <i62seroj@uco.es>
2026-08-24 19:07:13 +0200

    Update init lazy file

--- src/miml/classifier/miml/lazy/__init__.py
+++ src/miml/classifier/miml/lazy/__init__.py
@@ -2,12 +2,14 @@ from .miml_br_knn import MIMLBRkNN, ExtensionType
 from .miml_dgc import MIMLDGC
 from .miml_map_knn import MIMLMAPkNN
 from .miml_knn import MIMLkNN
+from .multi_instance_multi_label_knn import MultiInstanceMultiLabelKNN
 
 
 __all__ = [ 
     "MIMLBRkNN",
     "MIMLDGC",
-    "MIMLMAPkNN"
-    "MIMLkNN"
-    "ExtensionType"
+    "MIMLMAPkNN",
+    "MIMLkNN",
+    "MultiInstanceMultiLabelKNN",
+    "ExtensionType",
 ]
